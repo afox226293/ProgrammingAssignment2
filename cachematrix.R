a529f611aa953590f2cb979874ea08f1cd054e2f
afox226293 <afox226293@gmail.com>
2017-03-15 19:43:51 +0000

    altered cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following 2 functions work together to pass a matrix and return its inverse using
+## solve().  As solve() can be a costly computation cacheSolve() computes the inverse of
+## the matrix and stores it in memory within the parent directory allowing for it to be 
+## rapidly called upon in subsequent runs of the function.  When passed a matrix as 
+## argument x, makeCacheMatrix() returns a list of 4 functions which can then be passed
+## to cacheSolve().
 
-## Write a short comment describing this function
+## makeCacheMatrix() accepts a matrix as argument x returns an object of type list 
+## that can be passed to function cacheSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(a) {
+                m <<- NULL
+                x <<- a
+        }
+        get <- function() {
+                x
+        }
+        setSolve <- function(solve) {
+                m <<- solve
+        }
+        getSolve <- function() {
+                m
+        }
+        list(set = set,
+             get = get,
+             setSolve = setSolve,
+             getSolve = getSolve)
+        
+}
 
+## cacheSolve() accepts arguments of type list returned by makeCacheMatrix() and returns the 
+## inverse of the matrix oringally passed to makeCacheMatrix(). It also stores the result
+## in memory so if function cacheSolve() is called with the same argument again the result
+## will be returned from the cache as opposed to being computed with solve().
+
+cacheSolve <- function(x, ...){
+        m <- x$getSolve()
+        if(!is.null(m)) {
+                message("Getting cached data")
+                return(m)
+        } else {
+                mat <- x$get()
+                m <- solve(mat, ...)
+                x$setSolve(m)
+                return(m)
+        }
+        
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
